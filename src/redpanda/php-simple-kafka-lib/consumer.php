e12b047cba773f4b275552bd3bd7c1fb7844a3f7
nick <nickjobszh@gmail.com>
2021-04-21 07:46:06 +0200

    add dedicated redpanda example folder

---
+++ src/redpanda/php-simple-kafka-lib/consumer.php
@@ -0,0 +1,68 @@
+<?php
+
+require_once('../vendor/autoload.php');
+
+use PhpKafka\Consumer\KafkaConsumerBuilder;
+use PhpKafka\Exception\KafkaConsumerConsumeException;
+use PhpKafka\Exception\KafkaConsumerEndOfPartitionException;
+use PhpKafka\Exception\KafkaConsumerTimeoutException;
+
+// Get consumer Builder instance
+$builder = KafkaConsumerBuilder::create();
+
+// Configure consumer
+$consumer = $builder->withAdditionalConfig(
+    [
+        // start at the very beginning of the topic when reading for the first time
+        'auto.offset.reset' => 'earliest',
+
+        // will be visible in broker logs
+        'client.id' => 'php-kafka-lib-high-level-consumer',
+
+        // SSL settings
+        //'security.protocol' => 'ssl',
+        //'ssl.ca.location' => __DIR__.'/../../../keys/ca.pem',
+        //'ssl.certificate.location' => __DIR__.'/../../../keys/apl_stage.cert',
+        //'ssl.key.location' => __DIR__.'/../../../keys/apl_stage.key',
+
+        // SASL settings
+        //'sasl.mechanisms' => '',
+        //'ssl.endpoint.identification.algorithm' => 'https',
+        //'sasl.username' => '',
+        //'sasl.password' => '',
+
+        // Add additional output if you need to debug a problem
+        // 'log_level' => (string) LOG_DEBUG,
+        // 'debug' => 'all'
+    ]
+)
+    ->withAdditionalBroker('kafka:9096')
+    ->withConsumerGroup('php-kafka-lib-high-level-consumer')
+    ->withSubscription('php-kafka-lib-test-topic')
+    ->build();
+
+$consumer->subscribe();
+
+while (true) {
+    try {
+        $message = $consumer->consume(10000);
+    } catch (KafkaConsumerTimeoutException|KafkaConsumerEndOfPartitionException $e) {
+        echo 'Didn\'t receive any messages, waiting for more...' . PHP_EOL;
+        continue;
+    } catch (KafkaConsumerConsumeException $e) {
+        echo $e->getMessage() . PHP_EOL;
+        continue;
+    }
+
+    echo sprintf(
+            'Read message with key:%s payload:%s topic:%s partition:%d offset:%d headers:%s',
+            $message->getKey(),
+            $message->getBody(),
+            $message->getTopicName(),
+            $message->getPartition(),
+            $message->getOffset(),
+            implode(',', $message->getHeaders())
+        ) . PHP_EOL;
+
+    $consumer->commit($message);
+}
---
+++ src/redpanda/pure-php/consumer.php
@@ -0,0 +1,74 @@
+<?php
+
+use SimpleKafkaClient\Configuration;
+use SimpleKafkaClient\Consumer;
+
+$conf = new Configuration();
+// will be visible in broker logs
+$conf->set('client.id', 'pure-php-high-level-consumer');
+// set consumer group, e.g. <my-application-name>-consumer
+$conf->set('group.id', 'pure-php-high-level-consumer');
+// set broker
+$conf->set('metadata.broker.list', 'kafka:9096');
+// don't auto commit, give the application the control to do that (default is: true)
+$conf->set('enable.auto.commit', 'false');
+// start at the very beginning of the topic when reading for the first time
+$conf->set('auto.offset.reset', 'earliest');
+// Get eof code instead of null
+$conf->set('enable.partition.eof', 'true');
+
+// SASL Authentication
+//$conf->set('sasl.mechanisms', '');
+//$conf->set('ssl.endpoint.identification.algorithm', 'https');
+//$conf->set('sasl.username', '');
+//$conf->set('sasl.password', '');
+
+// SSL Authentication
+//$conf->set('security.protocol', 'ssl');
+//$conf->set('ssl.ca.location', __DIR__.'/../../../keys/ca.pem');
+//$conf->set('ssl.certificate.location', __DIR__.'/../../../keys/kafka.cert');
+//$conf->set('ssl.key.location', __DIR__.'/../../../keys/kafka.key');
+
+// Add additional output if you need to debug a problem
+// $conf->set('log_level', (string) LOG_DEBUG);
+// $conf->set('debug', 'all');
+
+// Create high level consumer
+$consumer = new Consumer($conf);
+
+// Subscribe to one or multiple topics
+$consumer->subscribe(['pure-php-test-topic', 'pure-php-transactional-test-topic']);
+
+while (true) {
+    // Try to consume messages for the given timout (20s)
+    $message = $consumer->consume(20000);
+
+    if (RD_KAFKA_RESP_ERR__PARTITION_EOF === $message->err) {
+        echo 'Reached end of partition, waiting for more messages...' . PHP_EOL;
+        continue;
+    } else if (RD_KAFKA_RESP_ERR__TIMED_OUT === $message->err) {
+        echo 'Timed out without receiving a new message, waiting for more messages...' . PHP_EOL;
+        continue;
+    } else if (RD_KAFKA_RESP_ERR_NO_ERROR !== $message->err) {
+        echo kafka_err2str($message->err) . PHP_EOL;
+        continue;
+    }
+
+    echo sprintf(
+        'Read message with key:%s payload:%s topic:%s partition:%d offset:%d',
+        $message->key,
+        $message->payload,
+        $message->topic_name,
+        $message->partition,
+        $message->offset
+    ) . PHP_EOL;
+    // Here is where you do your business logic to process your message
+    // after you have done so, commit the message offset to the broker
+
+    // commit the message(s) offset synchronous back to the broker
+    $consumer->commit($message);
+
+    // you can also commit the message(s) offset in an async manner, which is slightly faster
+    // but poses of course the challenge of handling errors in an async manner as well
+    //$consumer->commitAsync($message);
+}
